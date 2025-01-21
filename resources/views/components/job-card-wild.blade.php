f6ca4fa04121c57c1ec9b0755e4fb29aba5cc976
DevAhmedKhalil <dev.akhalil01@gmail.com>
2025-01-21 23:11:15 +0200

    Add employer-logo.blade

--- resources/views/components/job-card-wild.blade.php
+++ resources/views/components/job-card-wild.blade.php
@@ -1,14 +1,15 @@
 <div class="p-4 bg-white/5 rounded-xl flex gap-6">
 
     <div>
-        <img src="https://picsum.photos/seed/{{ rand(0, 100000) }}/100/100" alt="job img" class="rounded-xl">
+        <x-employer-logo/>
     </div>
 
-    <div class="flex-1">
+    <div class="flex-1 flex flex-col">
         <a href="#" class="self-start text-sm text-gray-400">Laracasts</a>
 
-        <h3>Video Producer</h3>
-        <p>Full Time - From $60,000</p>
+        <h3 class="font-bold text-xl mt-3">Video Producer</h3>
+
+        <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</p>
     </div>
 
     <div>
--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,10 +1,11 @@
-<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+<div
+    class="p-4 bg-white/5 rounded-xl flex flex-col text-center border border-transparent hover:border-blue-800 group">
 
     <div class="self-start">Laracasts</div>
 
     <div class="py-8 font-bold">
-        <h3>Video Producer</h3>
-        <p>Full Time - From $60,000</p>
+        <h3 class="group-hover:text-blue-600 text-xl">Video Producer</h3>
+        <p class="text-sm mt-4">Full Time - From $60,000</p>
     </div>
 
     <div class="flex justify-between items-center mt-auto">
@@ -13,8 +14,9 @@
             <x-tag>Tag</x-tag>
             <x-tag>Tag</x-tag>
         </div>
+        <x-employer-logo :width="42"/>
 
-        <img src="https://placehold.it/42/42" alt="job img" class="rounded-xl">
     </div>
 
 </div>
+
