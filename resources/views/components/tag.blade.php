412fc6b5137cb1544e59eb0bcb82f8e0aa636df2
Ahmed Khalil <109757454+DevAhmedKhalil@users.noreply.github.com>
2025-02-11 18:10:08 +0200

    make panel.blade.php .

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,2 +1,11 @@
+@props(['size'=>'base'])
+
+@php
+    if($size==='base'){
+
+
+    }
+if ($size === '')
+@endphp
 <a href="#"
    class="bg-white/10 hover:bg-white/25 px-3 py-1 rounded-xl text-2xs font-bold transition-colors duration-300">{{ $slot }}</a>
