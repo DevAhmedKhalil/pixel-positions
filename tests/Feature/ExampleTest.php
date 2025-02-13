aa2b95c90464c8c6b9e703b12668e62daa7a4a8b
Ahmed Khalil <109757454+DevAhmedKhalil@users.noreply.github.com>
2025-02-13 20:51:12 +0200

    Make JobTest.php & make tags & job_tag Tables.

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,10 @@
 <?php
 
 it('returns a successful response', function () {
-    $response = $this->get('/');
+    // If your / route is protected by middleware (e.g., authentication),
+    // your test might fail with a 403 Forbidden or 302 Redirect response.
+    // Using withoutMiddleware() will disable middleware and allow the test to proceed without restrictions.
+    $response = $this->withoutMiddleware()->get('/');
 
     $response->assertStatus(200);
 });
