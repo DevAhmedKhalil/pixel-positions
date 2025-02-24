f4c8e81c5b84d3305c4a2eaad841931b7853f73b
yasmin282 <y282ahmed@gmail.com>
2025-02-24 14:33:05 +0200

    test pc push

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -27,7 +27,7 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request)
     {
-        return view('auth.register');
+//        return view('auth.register');
     }
 
     /**
