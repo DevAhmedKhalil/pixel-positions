9712bea40bf3a56b2164e72f2c70ae19e7eb75ea
Ahmed Khalil <109757454+DevAhmedKhalil@users.noreply.github.com>
2025-02-26 20:26:18 +0200

    Finish Search Functions

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Job;
+
 class SearchController extends Controller
 {
     public function __invoke()
     {
+//        dd(request('q'));
+        $jobs = Job::where('title', 'like', '%' . request('q') . '%')->get();
+
+        return view('results', ['jobs' => $jobs]);
 
     }
 }
